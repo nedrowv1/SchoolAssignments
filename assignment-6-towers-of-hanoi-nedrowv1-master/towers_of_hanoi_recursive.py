4752bdd1dae51d2943dd859643ad06eb04adda2d
Nessa <42817020+nedrowv1@users.noreply.github.com>
2019-09-30 13:08:38 -0400

    Update to how pillars are printed

--- assignment-6-towers-of-hanoi-nedrowv1-master/towers_of_hanoi_recursive.py
+++ assignment-6-towers-of-hanoi-nedrowv1-master/towers_of_hanoi_recursive.py
@@ -11,7 +11,7 @@ def define_pillar_row(pillar, disk_index, ttl_disks, endchar):
     """calculate the amount of white space needed to keep rows aligned"""
     if len(pillar) > disk_index:
         buffer = " " * (ttl_disks - len(pillar[disk_index]))
-        print_disk = pillar[disk_index] + buffer
+        print_disk = pillar[disk_index]  + buffer
     else:
         print_disk = ' ' * ttl_disks
     print(print_disk, end=endchar)
@@ -27,7 +27,7 @@ def print_pillar(disks, pillar_1, pillar_2, pillar_3):
     sleep(3)
 
 
-def move_towers(key_1, key_2, strt_disks):
+def move_towers(key_1, key_2, strt_disks, air):
     """inserts, pops and prints the movement of the top disk of a given pillar
     to the top of another"""
     # global variable used for manipulation of lists - because otherwise
@@ -43,7 +43,7 @@ def move_towers(key_1, key_2, strt_disks):
     elif key_1 == 3:
         move_tower = END
     else:
-        raise ValueError("The value passed in key_1 was improperly defined")
+        raise ValueError("The value passed in key_1 is invalid")
 
     if key_2 == 1:
         drop_tower = START
@@ -52,9 +52,21 @@ def move_towers(key_1, key_2, strt_disks):
     elif key_2 == 3:
         drop_tower = END
     else:
-        raise ValueError("The value passed in key_2 was improperly defined")
+        raise ValueError("The value passed in key_2 is invalid")
     # move first item from move from tower and put in first place of drop to tower
-    drop_tower.insert(0, move_tower.pop(0))
+    for i, value in enumerate(move_tower):
+        if value != air:
+            move_disk = value
+            move_tower[i] = air
+            break
+    for i, value in enumerate(drop_tower):
+        if value != air:
+            drop_tower[i-1] = move_disk
+            break
+        elif i == len(drop_tower) -1:
+            drop_tower[i] = move_disk
+            break           
+                    
     print_pillar(strt_disks, START, MID, END)
 
 
@@ -64,7 +76,7 @@ def towers_of_hanoi(tower1, tower3, tower2, num_disks, tot_disks):
     if num_disks > 0:
         new_height = num_disks - 1
         towers_of_hanoi(tower1, tower2, tower3, new_height, tot_disks)
-        move_towers(tower1, tower3, tot_disks)
+        move_towers(tower1, tower3, tot_disks, " " * tot_disks)
         towers_of_hanoi(tower2, tower3, tower1, new_height, tot_disks)
 
 
