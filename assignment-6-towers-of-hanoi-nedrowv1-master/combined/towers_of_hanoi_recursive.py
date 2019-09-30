ffcd6fc1d08cd1970b1821aa88c600b520c69e76
Nessa <42817020+nedrowv1@users.noreply.github.com>
2019-09-30 15:19:46 -0400

    Update towers_of_hanoi_recursive.py

--- assignment-6-towers-of-hanoi-nedrowv1-master/combined/towers_of_hanoi_recursive.py
+++ assignment-6-towers-of-hanoi-nedrowv1-master/combined/towers_of_hanoi_recursive.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 14:52:10 2019
+
+@author: vnedrow
+"""
 from towers_of_hanoi_joint import move_towers
 
-def towers_of_hanoi(tower1, tower3, tower2, num_disks, tot_disks):
+def towers_of_hanoi(tower1, tower3, tower2, num_disks, tot_disks, START, MID, END):
     """for each disk in n- 1 disks, move disks from pillar 1 to pillar 2, 
     and then from pillar 2 to pillar3"""
     if num_disks > 0:
         new_height = num_disks - 1
-        towers_of_hanoi(tower1, tower2, tower3, new_height, tot_disks)
-        move_towers(tower1, tower3, tot_disks, " " * tot_disks)
-        towers_of_hanoi(tower2, tower3, tower1, new_height, tot_disks)
+        towers_of_hanoi(tower1, tower2, tower3, new_height, tot_disks, START, MID, END)
+        move_towers(tower1, tower3, tot_disks, " " * tot_disks, START, MID, END)
+        towers_of_hanoi(tower2, tower3, tower1, new_height, tot_disks, START, MID, END)
